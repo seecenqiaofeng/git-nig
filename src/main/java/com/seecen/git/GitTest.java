f84501ec0a3e89b95fa3372c0ac3756e20aa4979
chenyue <291975311@qq.com>
2022-07-26 17:06:30 +0800

    second commit

---
+++ src/main/java/com/seecen/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.seecen.git;
+
+/**
+ * @author cystart
+ * @date 2022/7/26 - 17:03
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+    }
+}
